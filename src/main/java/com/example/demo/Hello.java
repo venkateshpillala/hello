8fa66b10e34d7289e5f85b201e5a4b68e51b6b03
venkateshpillala <153274380+venkateshpillala@users.noreply.github.com>
2025-04-23 18:03:42 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class Hello {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello";
+	}
+}
